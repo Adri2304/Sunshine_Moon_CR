d7770e4e5c06399bcd8adc8eb2fde3e7ec5f914b
ADRIAN.GRAJALMIRANDA <adrian.grajalmiranda@ucr.ac.cr>
2024-11-10 23:48:18 -0600

    Se creo el controlador para autenticacion, ademas de la intalacion de dependencias, configuracion del servidor para manejar la autenticacion y autorizacion con JWT

---
+++ capa_negocio/Controllers/AuthController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Runtime.InteropServices;
+using System.Security.Claims;
+using System.Text;
+
+namespace capa_negocio.Controllers
+{
+    [ApiController]
+    [Route("autenticacion")]
+    public class AuthController : ControllerBase
+    {
+        private IConfiguration Configuracion;
+        public AuthController(IConfiguration configuracion)
+        {
+            this.Configuracion = configuracion;
+        }
+
+        [HttpGet]
+        [Route("autenticar")]
+        public async Task<ActionResult> Autenticar([FromBody] Dictionary<string, string> body)
+        {
+            var tokens = GenerateJWTToken("2", "2", "Adrian@gmail.com");
+            return Ok(tokens);
+        }
+        
+     
+
+        private Dictionary<string, string> GenerateJWTToken(string id, string rol, string correo)
+        {
+            var tokens = new Dictionary<string, string>();
+            var claims = new List<Claim> {
+                new Claim(ClaimTypes.NameIdentifier, id),
+                new Claim(ClaimTypes.Role, rol),
+                new Claim(ClaimTypes.Email, correo),
+            };
+            var jwtToken = new JwtSecurityToken(
+                issuer: Configuracion["JwtSettings:JwtIssuer"],
+                audience: Configuracion["JwtSettings:JwtAudience"],
+                claims: claims,
+                notBefore: DateTime.UtcNow,
+                expires: DateTime.UtcNow.AddMinutes(3),
+                signingCredentials: new SigningCredentials(
+                    new SymmetricSecurityKey(
+                       Encoding.UTF8.GetBytes(Configuracion["JwtSettings:JwtSecret"])
+                        ),
+                    SecurityAlgorithms.HmacSha256Signature)
+                );
+
+            var jwtRefreshToken = new JwtSecurityToken(
+                issuer: Configuracion["JwtSettings:JwtIssuer"],
+                audience: Configuracion["JwtSettings:JwtAudience"],
+                claims: new List<Claim> { new Claim(ClaimTypes.NameIdentifier, id) },
+                notBefore: DateTime.UtcNow,
+                expires: DateTime.UtcNow.AddDays(2),
+                signingCredentials: new SigningCredentials(
+                    new SymmetricSecurityKey(
+                       Encoding.UTF8.GetBytes(Configuracion["JwtSettings:JwtSecret"])
+                        ),
+                    SecurityAlgorithms.HmacSha256Signature)
+                );
+
+            tokens.Add("accessToken", new JwtSecurityTokenHandler().WriteToken(jwtToken));
+            tokens.Add("refreshToken", new JwtSecurityTokenHandler().WriteToken(jwtRefreshToken));
+            return tokens;
+        }
+    }
+}
--- capa_negocio/Program.cs
+++ capa_negocio/Program.cs
@@ -1,15 +1,36 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+builder.Services.AddAuthentication(cfg => {
+    cfg.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    cfg.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    cfg.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x => {
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = false;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8
+            .GetBytes(builder.Configuration["JwtSettings:JwtSecret"])
+        ),
+        ValidateIssuer = true,
+        ValidIssuer = builder.Configuration["JwtSettings:JwtIssuer"],
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["JwtSettings:JwtAudience"],
+        ClockSkew = TimeSpan.Zero
+    };
+});
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
